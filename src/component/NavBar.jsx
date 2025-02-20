e18a378aafed4aacbd35081fba7aa212f369dc20
Adeyemi Pelumi <adeyemipelumi1980@gmail.com>
2025-02-20 12:15:41 +0100

    new

--- src/component/NavBar.jsx
+++ src/component/NavBar.jsx
@@ -1,66 +1,77 @@
-import React, { use } from "react";
-// import { Link } from "react-router-dom";
+import React, { useState } from "react";
 import "../css-component/navbar.css";
 import navlogo from "../assets/movie-logo.png";
 import magnifying from "../assets/magnifying-glass.png";
 import bell from "../assets/bell.png";
 import user from "../assets/user.png";
 import sun from "../assets/sun.png";
+import menuIcon from "../assets/menu-line.png"; // Add a hamburger icon
+import closeIcon from "../assets/close-large-line.png"; // Add a close icon
 
 const NavBar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  // Toggle the menu
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <>
-      {/* navbar container */}
       <div className="nav-bar-container">
-        {/* nav-logo-child */}
-        <div className="nav-logo-child">
-          {/* nav-bar-logo */}
+        {/* Hamburger Button for Mobile */}
+        <button className="hamburger" onClick={toggleMenu}>
+          <img src={isOpen ? closeIcon : menuIcon} alt="menu" />
+        </button>
+
+        {/* Navbar Content */}
+        <div className={`nav-logo-child ${isOpen ? "open" : ""}`}>
+          {/* Logo */}
           <div className="nav-bar-logo">
-            <img src={navlogo} alt="" />
+            <img src={navlogo} alt="Logo" />
           </div>
-          {/*end of  nav-bar-logo */}
-          {/* nav-link */}
+
+          {/* Navigation Links */}
           <div className="Nav-link">
             <ul>
               <li>
                 <a href="/">Home</a>
               </li>
               <li>
-                <a href="">Pricing</a>
+                <a href="#">Pricing</a>
               </li>
               <li>
-                <a href="">Movies</a>
+                <a href="#">Movies</a>
               </li>
               <li>
-                <a href="">Series</a>
+                <a href="#">Series</a>
               </li>
               <li>
-                <a href="">Collection</a>
+                <a href="#">Collection</a>
               </li>
               <li>
-                <a href="">FAQ</a>
+                <a href="#">FAQ</a>
               </li>
             </ul>
           </div>
-          {/* end of nav-link */}
+
+          {/* Icons Section */}
           <div className="nav-left">
             <div className="left-1">
-              <img src={magnifying} alt="" />
+              <img src={magnifying} alt="search" />
             </div>
             <div className="left-1">
-              <img src={bell} alt="" />
+              <img src={bell} alt="notifications" />
             </div>
             <div className="left-1">
-              <img src={user} alt="" />
+              <img src={user} alt="user" />
             </div>
             <div className="left-1">
-              <img src={sun} alt="" />
+              <img src={sun} alt="theme" />
             </div>
           </div>
         </div>
-        {/* end of Nav-logo-child */}
       </div>
-      {/* end of navbar container */}
     </>
   );
 };
