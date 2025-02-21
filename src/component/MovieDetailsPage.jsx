0a11596b40ff7f886292f97775c29dbd7d50c27e
Adeyemi Pelumi <adeyemipelumi1980@gmail.com>
2025-02-21 15:41:37 +0100

    new

--- src/component/MovieDetailsPage.jsx
+++ src/component/MovieDetailsPage.jsx
@@ -44,78 +44,81 @@ const MovieDetailsPage = () => {
   return (
     <>
       <NavBar />
-      <div className="movie-details-container">
-        <div
-          className="movie-banner"
-          style={{
-            backgroundImage: `url(https://image.tmdb.org/t/p/original${movie.backdrop_path})`,
-            height: "809px",
-            width: "100%",
-          }}
-        >
-          <div className="banner-de">
-            <h1 className="detaails-ti">{movie.title}</h1>
-            <p className="date">
-              {movie.release_date} - {movie.runtime} mins {movie.origin_country}
-            </p>
-            <p className="vote">
-              {stars.map((star, index) => (
-                <span key={index}>{star}</span> // Dynamically show stars ⭐⭐⭐⭐⭐⭐⭐
-              ))}
-            </p>
-            <img src={like} alt="" className="like" />
-          </div>
-          <div className="play-sect">
-            <button className="play1">Watch Now</button>
-            <button className="play2">Preview</button>
-          </div>
-        </div>
-        <div className="movie-info">
-          <div className="details">
-            <h2>About {movie.title}</h2>
-            <p>{movie.overview}</p>
-            <div className="genre">
-              <h1>Genres</h1>
-              <div className="gen-bu">
-                {movie.genres.map((genre) => (
-                  <button key={genre.id}>{genre.name}</button>
+      <div className="movie-container">
+        <div className="movie-details-container">
+          <div
+            className="movie-banner"
+            style={{
+              backgroundImage: `url(https://image.tmdb.org/t/p/original${movie.backdrop_path})`,
+              height: "809px",
+              width: "100%",
+            }}
+          >
+            <div className="banner-de">
+              <h1 className="detaails-ti">{movie.title}</h1>
+              <p className="date">
+                {movie.release_date} - {movie.runtime} mins{" "}
+                {movie.origin_country}
+              </p>
+              <p className="vote">
+                {stars.map((star, index) => (
+                  <span key={index}>{star}</span> // Dynamically show stars ⭐⭐⭐⭐⭐⭐⭐
                 ))}
+              </p>
+              <img src={like} alt="" className="like" />
+            </div>
+            <div className="play-sect">
+              <button className="play1">Watch Now</button>
+              <button className="play2">Preview</button>
+            </div>
+          </div>
+          <div className="movie-info">
+            <div className="details">
+              <h2>About {movie.title}</h2>
+              <p>{movie.overview}</p>
+              <div className="genre">
+                <h1>Genres</h1>
+                <div className="gen-bu">
+                  {movie.genres.map((genre) => (
+                    <button key={genre.id}>{genre.name}</button>
+                  ))}
+                </div>
+              </div>
+              <div className="pro">
+                <h1>Production Company</h1>
+              </div>
+              <div className="chractor">
+                {movie.production_companies &&
+                movie.production_companies.length > 0 ? (
+                  movie.production_companies.map((company) =>
+                    company.logo_path ? (
+                      <div key={company.id} className="company-logo">
+                        <img
+                          src={`https://image.tmdb.org/t/p/w500${company.logo_path}`}
+                          alt={company.name}
+                        />
+                      </div>
+                    ) : null
+                  )
+                ) : (
+                  <p>No production companies found</p>
+                )}
               </div>
             </div>
-            <div className="pro">
-              <h1>Production Company</h1>
-            </div>
-            <div className="chractor">
-              {movie.production_companies &&
-              movie.production_companies.length > 0 ? (
-                movie.production_companies.map((company) =>
-                  company.logo_path ? (
-                    <div key={company.id} className="company-logo">
-                      <img
-                        src={`https://image.tmdb.org/t/p/w500${company.logo_path}`}
-                        alt={company.name}
-                      />
-                    </div>
-                  ) : null
-                )
-              ) : (
-                <p>No production companies found</p>
-              )}
-            </div>
           </div>
         </div>
-      </div>
-      <Suggestion />
-      <div className="comment">
-        <div className="comment-head">
-          <h1>Comments</h1>
-          <p> See More</p>
-        </div>
-        <div className="cards">
-          <img src={card1} alt="" />
-          <img src={card2} alt="" />
-          <img src={card3} alt="" />
-          <img src={card4} alt="" />
+        <Suggestion />
+        <div className="comment">
+          <div className="comment-head">
+            <h1>Comments</h1>
+            <p> See More</p>
+          </div>
+          <div className="cards">
+            <img src={card1} alt="" />
+            <img src={card2} alt="" />
+            <img src={card3} alt="" />
+            <img src={card4} alt="" />
+          </div>
         </div>
       </div>
       <FooterDetails />
